4e83b0ad8c6dd26be044983fb169900da565a9a3
Sebastian Degerman <sebbed89@hotmail.com>
2015-12-26 11:49:20 +0100

    commit

--- public/js/default.js
+++ public/js/default.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$("#playBtn").click(function() {
 		window.location.replace("/game.html");
 	});
+	
+	$(".color-picker-group button").on("click", function() {
+		$(".color-btn").removeClass("active");
+		$(this).addClass("active");
+	});
 });
