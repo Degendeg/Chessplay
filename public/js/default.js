fb1f1e0d70924d0796eee7b2849cb5806bdf116b
Sebastian Degerman <sebbed89@hotmail.com>
2016-01-14 22:10:39 +0100

    commit

--- public/js/default.js
+++ public/js/default.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
 		localStorage.setItem('_chosenColor', chosenColor);
 	});
 	
-	$('#playBtn').removeAttr('href').css('color','grey');
+	$('#playBtn').removeAttr('href').css('opacity','0.5');
 	
 	$(".color-picker-group button").on("click", function() {
 		$(".color-btn").removeClass("active");
 		$(this).addClass("active");
 		if ($(this).hasClass('active') == true) {
-			$('#playBtn').attr("href", "game.html").css('color','black');
+			$('#playBtn').attr("href", "game.html").css('color','white').css('opacity','1');
 		}
 	});
 	
