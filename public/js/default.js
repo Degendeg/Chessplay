b8177719edbfc286d49b41a294b087342a2f632c
Sebastian Degerman <sebbed89@hotmail.com>
2016-03-17 22:03:07 +0100

    Removed randomize, added anonymous btn

--- public/js/default.js
+++ public/js/default.js
@@ -41,10 +41,8 @@ $(document).ready(function() {
     //do this every 10 secs
     window.setInterval(generateQuote, 10000);
     
-    $('#randomBtn').click(function () {
-        $.get( "https://randomuser.me/api/", function(data) {
-            $("#username").val(data.results[0].user.username);
-        });
+    $('#anonBtn').click(function () {
+        $("#username").val("Anonymous");
     });
 	
 	$('#playBtn').prop('disabled', true);
